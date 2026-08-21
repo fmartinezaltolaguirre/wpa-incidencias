98f3fe6c90117bc0c94cd9388646807b9ff70c4e
Fernando Martínez <96567345+fmartinezaltolaguirre@users.noreply.github.com>
2026-08-21 13:12:37 +0200

    fix: make application routes valid React component

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,16 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard";
+import Incidencias from "../pages/Incidencias";
 import NuevaIncidencia from "../pages/NuevaIncidencia";
 
-<Routes>
-  <Route path="/" element={<Dashboard />} />
-  <Route path="/incidencias" element={<Incidencias />} />
-  <Route path="/nueva" element={<NuevaIncidencia />} />
-</Routes>
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/incidencias" element={<Incidencias />} />
+      <Route path="/nueva" element={<NuevaIncidencia />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
